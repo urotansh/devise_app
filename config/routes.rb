8bbd36e9b7dc493b0e0e4a6ac09714f3277d41f8
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-18 02:35:35 +0000

    Add devise_for :users to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
-  devise_for :users
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
