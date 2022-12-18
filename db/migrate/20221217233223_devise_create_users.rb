e6ce8796e628b229d84de1748184e7fc14625134
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-18 02:57:47 +0000

    Add name and is_deleted column to devise migration file

--- db/migrate/20221217233223_devise_create_users.rb
+++ db/migrate/20221217233223_devise_create_users.rb
@@ -4,7 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
+      t.boolean :is_deleted,        null: false, default: false
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
