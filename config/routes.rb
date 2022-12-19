da0874956b1ef2e2825e4711e94125c5f6d5213d
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-19 18:57:46 +0900

    Update config.sign_out_all_scopes in devise.rb from true to false

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,14 @@ Rails.application.routes.draw do
     sessions: "public/sessions"
   }
   
-  #デバッグ用 GETメソッドサインアウト
+  #デバッグ用 GETメソッドサインアウト(users)
   devise_scope :user do
     get "/users/sign_out", to: "public/sessions#destroy"
+  end
+  
+  #デバッグ用 GETメソッドサインアウト(admin)
+  devise_scope :admin do
+    get "/admin/sign_out", to: "admin/sessions#destroy"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
