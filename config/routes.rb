c95078f00aba1211c0b6ddccff2a88d0087f660d
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-20 02:09:24 +0000

    Add guestuser login function

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,15 @@ Rails.application.routes.draw do
   }
   
   #デバッグ用 GETメソッドサインアウト(users)
+  #ゲストログイン用ルーティング
   devise_scope :user do
-    get "/users/sign_out", to: "public/sessions#destroy"
+    get "users/sign_out", to: "public/sessions#destroy"
+    post "users/guest_sign_in", to: "public/sessions#guest_sign_in"
   end
   
   #デバッグ用 GETメソッドサインアウト(admin)
   devise_scope :admin do
-    get "/admin/sign_out", to: "admin/sessions#destroy"
+    get "admin/sign_out", to: "admin/sessions#destroy"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
